c4f947b7ca35526592b74c3f96442d0308eb664f
naol17 <57032678+naol17@users.noreply.github.com>
2021-12-07 11:27:20 +0300

    many changes are made including adding additional library for the animations and diffrent styling formates

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -15,21 +15,6 @@
     // Initiate the wowjs
     new WOW().init();
 
-
-    // Back to top button
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function() {
-        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
     // Sticky Navbar
     $(window).scroll(function() {
         if ($(this).scrollTop() > 0) {
